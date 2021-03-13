3ade26de11e815edba66660ff65f14c82671a39f
Vijay Raja <jobprepnyu@gmail.com>
2021-03-13 00:25:54 -0500

    Create myPow.py

---
+++ Amazon/myPow.py
@@ -0,0 +1,14 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n<0:
+            return self.myPow(1/x, -n)
+        
+        half = self.myPow(x, n//2)
+        
+        if n%2 == 0:
+            return half*half
+    
+        else:
+            return half*half*x
